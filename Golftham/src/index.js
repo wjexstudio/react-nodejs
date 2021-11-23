39bf05de979b561393d33f5ef9068671a257945c
Wasin jex <wasin.jex@gmail.com>
2021-11-23 15:30:11 +0700

    add first app

---
+++ Golftham/src/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseApp = initializeApp({
+const firebaseAnalytics = getAnalytics(firebaseApp);
+
+    apiKey: "AIzaSyA5IV_mGQNmZuGb__TEIXAMUCSbgbuMN_8",
+    authDomain: "golftham-skateboard.firebaseapp.com",
+    projectId: "golftham-skateboard",
+    storageBucket: "golftham-skateboard.appspot.com",
+    messagingSenderId: "877404560002",
+    appId: "1:877404560002:web:e0f47b4e4de35f5819f29b",
+    measurementId: "G-PP5TCC689F"
+    });
